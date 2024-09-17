3a5014de41004b2bc07a516cb5221c5eea10e539
SANTOS  PEDRO BALTAZAR JOJ CANO <santospedrobaltazarjc02@gmail.com>
2024-09-16 19:30:20 -0600

    Ten Commit

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -10,21 +10,24 @@ function Gallery() {
   }, [t]);
 
   const gallery = [
-    { id: 1, name: t("home.love19"), imageSrc: '/img/img1.jpg' },
-    { id: 2, name: t("home.love20"), imageSrc: '/img/img2.jpg' },
-    { id: 3, name: t("home.love21"), imageSrc: '/img/img3.jpg' },
-    { id: 4, name: t("home.love22"), imageSrc: '/img/img4.jpg' },
-    { id: 5, name: t("home.love23"), imageSrc: '/img/img5.jpg' },
-    { id: 6, name: t("home.love24"), imageSrc: '/img/img6.jpg' },
-    { id: 7, name: t("home.love25"), imageSrc: '/img/img7.jpg' },
-    { id: 8, name: t("home.love26"), imageSrc: '/img/img8.jpg' },
-    { id: 9, name: t("home.love27"), imageSrc: '/img/img9.jpg' },
-    { id: 10, name: t("home.love28"), imageSrc: '/img/img10.jpg' },
-    { id: 11, name: t("home.love29"), imageSrc: '/img/img11.jpg' },
-    { id: 12, name: t("home.love30"), imageSrc: '/img/img12.jpg' },
-    { id: 13, name: t("home.love31"), imageSrc: '/img/img13.jpg' },
-    { id: 14, name: t("home.love32"), imageSrc: '/img/img14.jpg' },
-    { id: 15, name: t("home.love33"), imageSrc: '/img/img15.jpg' }
+    { id: 1, name: t("home.love19"), imageSrc: "/img/img1.jpg" },
+    { id: 2, name: t("home.love20"), imageSrc: "/img/img2.jpg" },
+    { id: 3, name: t("home.love21"), imageSrc: "/img/img3.jpg" },
+    { id: 4, name: t("home.love22"), imageSrc: "/img/img4.jpg" },
+    { id: 5, name: t("home.love23"), imageSrc: "/img/img5.jpg" },
+    { id: 6, name: t("home.love24"), imageSrc: "/img/img6.jpg" },
+    { id: 7, name: t("home.love25"), imageSrc: "/img/img7.jpg" },
+    { id: 8, name: t("home.love26"), imageSrc: "/img/img8.jpg" },
+    { id: 9, name: t("home.love27"), imageSrc: "/img/img9.jpg" },
+    { id: 10, name: t("home.love28"), imageSrc: "/img/img10.jpg" },
+    { id: 11, name: t("home.love29"), imageSrc: "/img/img11.jpg" },
+    { id: 12, name: t("home.love30"), imageSrc: "/img/img12.jpg" },
+    { id: 13, name: t("home.love31"), imageSrc: "/img/img13.jpg" },
+    { id: 14, name: t("home.love32"), imageSrc: "/img/img14.jpg" },
+    { id: 15, name: t("home.love33"), imageSrc: "/img/img15.jpg" },
+    { id: 16, name: t("home.love34"), imageSrc: "/img/img16.jpg" },
+    { id: 17, name: t("home.love35"), imageSrc: "/img/img17.jpg" },
+    { id: 18, name: t("home.love36"), imageSrc: "/img/img18.jpg" },
   ];
 
   return (
